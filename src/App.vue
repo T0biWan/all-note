fca19cc32c0261d5bd2617a9e4bd505c72304970
t0biwan <tobi.klatt@web.de>
2017-12-24 10:36:05 +0100

    added functionality to write and render one note

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <hero>all-note</hero> <!-- kommt später wahrschienlich weg !-->
+    <hero title="all-note"></hero>  <!-- kommt später wahrschienlich weg !-->
+    <textarea v-model="note_content"></textarea>
+    <aside class="preview" v-html="render_note"></aside>
   </div>
 </template>
 
@@ -13,7 +15,28 @@ export default {
   components: {
     'hero': hero,
   },
+
+  data() {
+    return {
+      note_content: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn."
+    };
+  },
+
+
+  computed: {
+    render_note () {
+      // return marked(this.note_content)
+      return this.note_content
+    },
+  },
 }
 </script>
 
-<style></style>
+<style>
+textarea {
+  resize: none;
+  border: none;
+  box-sizing: border-box;
+  font-family: monospace;
+}
+</style>
