1b14fdb2bedac8187b077a330a68aff560bd9edd
t0biwan <tobi.klatt@web.de>
2017-12-24 11:45:58 +0100

    multiple notes

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <hero title="all-note"></hero>  <!-- kommt später wahrschienlich weg -->
     <aside></aside> <!-- display all notes -->
     <div> <!-- Toolbar -->
-      <button @click="add_note">Add note</button>
+      <button @click="addNote">Add note</button>
     </div>
     <div> <!-- list of notes -->
-      <div v-for="note of notes">{{note.title}}</div>
+      <div v-for="note of notes" @click="selectNote(note)">{{note.title}}</div>
     </div>
-    <textarea v-model="note_content"></textarea>
-    <aside v-html="render_note"></aside>
+    <textarea v-model="selectedNote.content"></textarea>
+    <aside v-html="notePreview"></aside>
   </div>
 </template>
 
@@ -25,43 +25,102 @@ export default {
 
   data() {
     return {
-      note_content: localStorage.getItem('note_content') || "Ph'nglui mglw'nafh **Cthulhu** R'lyeh wgah'nagl fhtagn.",
-      notes: [],
+      // content: localStorage.getItem('content') || "Ph'nglui mglw'nafh **Cthulhu** R'lyeh wgah'nagl fhtagn.",
+      notes: JSON.parse(localStorage.getItem('notes')) || [],
+      // selected_note_id: null,
+      selectedId: localStorage.getItem('selected-id') || null,
     };
   },
 
   computed: {
-    render_note () {
-      // return marked(this.note_content)
-      return this.note_content
+    // render_note () {
+    //   // return marked(this.content)
+    //   return this.content
+    // },
+    //
+    // selected_note () {
+    //   return this.notes.find(note => note.id === this.selected_note_id)
+    // },
+    selectedNote () {
+      // We return the matching note with selectedId
+      return this.notes.find(note => note.id === this.selectedId)
+    },
+
+    notePreview () {
+      // Markdown rendered to HTML
+      // return this.selectedNote ? marked(this.selectedNote.content) : ''
+      return this.selectedNote.content;
     },
   },
 
   methods: {
-    save_note (val) {
-      console.log('saving note:', val)
-      localStorage.setItem('note_content', val)
+    // save_note (val) {
+    //   console.log('saving note:', val)
+    //   localStorage.setItem('content', val)
+    // },
+    //
+    // select_note_id (note) {
+    //   this.selected_note = note.id
+    // },
+    //
+    // add_note () {
+    //   const time = Date.now()
+    //   // Default new note
+    //   const note = {
+    //     id: String(time),
+    //     title: 'New note ' + (this.notes.length + 1),
+    //     content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+    //     created: time,
+    //     favorite: false,
+    //   }
+    //   // Add to the list
+    //   this.notes.push(note)
+    // },
+
+    selectNote (note) {
+      // This will update the 'selectedNote' computed property
+      this.selectedId = note.id
     },
 
-    add_note () {
+    // Add a note with some default content and select it
+    addNote () {
       const time = Date.now()
       // Default new note
       const note = {
         id: String(time),
         title: 'New note ' + (this.notes.length + 1),
-        note_content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+        content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
         created: time,
         favorite: false,
       }
-      // Add to the list
+      // Add
       this.notes.push(note)
+      // Select
+      this.selectNote(note)
+    },
+
+    saveNotes () {
+      // Don't forget to stringify to JSON before storing
+      localStorage.setItem('notes', JSON.stringify(this.notes))
+      console.log('Notes saved!', new Date())
     },
   },
 
   watch: {
-    note_content: {
-      handler: 'save_note',
+    // content: {
+    //   handler: 'save_note',
+    //   deep: true,
+    // },
+    // When our notes change, we save them
+    notes: {
+      // The method name
+      handler: 'saveNotes',
+      // We need this to watch each note's properties inside the array
       deep: true,
+    },
+    // Let's save the selection too
+    selectedId (val, oldVal) {
+      localStorage.setItem('selected-id', val)
     },
   },
 }
